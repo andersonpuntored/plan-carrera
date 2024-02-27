2ec37089369e2304637c53edb317f61b393a8a79
andersonpuntored <anderson.garcia@puntored.co>
2024-02-27 13:08:31 -0500

    [Advance] -Se genera controlador -Se imprime en pantalla el retorno de la peticion

--- desarrollo-web/pages/users/index.vue
+++ desarrollo-web/pages/users/index.vue
@@ -1,3 +1,32 @@
 <template>
-  <h1>USERS</h1>
+  <div>
+    <h1>Usuarios</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user }}
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import {getUsers} from "~/api/controllers/users.controller.ts"
+
+export default {
+  setup() {
+    const users = ref([])
+
+    onMounted(async () => {
+      try {
+        const fetchedUsers = await getUsers()
+        console.log('fetchedUsers', fetchedUsers)
+        users.value = fetchedUsers
+      } catch (error) {
+        console.error('Error fetching users:', error)
+      }
+    });
+    return { users }
+  }
+}
+</script>
