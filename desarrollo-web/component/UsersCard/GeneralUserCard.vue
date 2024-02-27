25ad1a810a724eda59496569d42fa32a2d2dfed6
andersonpuntored <anderson.garcia@puntored.co>
2024-02-27 14:16:26 -0500

    [Advance] -GeneralUserCard: mejoras

--- desarrollo-web/component/UsersCard/GeneralUserCard.vue
+++ desarrollo-web/component/UsersCard/GeneralUserCard.vue
@@ -1,15 +1,17 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="user in users" :key="user.id">
-        <img :src="user.avatar" alt="Avatar" width="100" height="100">
-        <div>{{ user.last_name }}, {{ user.first_name }}</div>
-        <div>{{ user.email }}</div>
-        <div>{{ user.username }}</div>
-        <div>{{ user.avatar }}</div>
-
-      </li>
-    </ul>
+  <div v-for="user in users"
+       :key="user.id"
+       class="general-user-card-container"
+  >
+    <img :src="user.avatar"
+         alt="Avatar"
+         class="general-user-card-img"
+    >
+    <section class="general-user-card-section">
+      <div>{{ user.last_name }}, {{ user.first_name }}</div>
+      <div>{{ user.email }}</div>
+      <div>{{ user.username }}</div>
+    </section>
   </div>
 </template>
 
@@ -34,3 +36,23 @@ export default {
   }
 }
 </script>
+<style>
+.general-user-card-container {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  border: solid 1px black;
+  border-radius: 10px;
+  margin: 10px;
+}
+.general-user-card-img{
+  height: 100px;
+  width: auto;
+  border: solid 1px black;
+  margin: 10px;
+}
+.general-user-card-section {
+  display: flex;
+  flex-direction: column;
+}
+</style>
